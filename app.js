411e23d13752dcaff1c5e9144852220342e0f8f6
SJ Wu <wxxsij001@myuct.ac.za>
2021-03-25 22:16:15 +0200

    commit 7: adds css and javascript functionality to CVT tracker app by editing app.js and moving scripts and styles to public folder

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const router = express.Router();
+//serve static fields in express
+app.use(express.static(path.join(__dirname,"public"))); //then e.g. this will work on http://localhost:3000/images/firefox-icon.png
 
 router.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/views/index.html"));
