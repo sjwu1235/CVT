556a308f1332b720c8bc2f1d7f3fead8df520c3a
SJ Wu <wxxsij001@myuct.ac.za>
2021-04-01 12:50:50 +0200

    Commit 8: accesses and writies to a database via sqlite3.

--- app.js
+++ app.js
@@ -1,21 +1,113 @@
+//extra pass to template for status rewatch julian for reauirement
+//ejs specific syntax to grab data from submission
 const express = require("express");
-const app = express();
 const path = require("path");
-const router = express.Router();
-//serve static fields in express
-app.use(express.static(path.join(__dirname,"public"))); //then e.g. this will work on http://localhost:3000/images/firefox-icon.png
+const sqlite3 = require("sqlite3").verbose(); //to use database
 
-router.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/views/index.html"));
-  //__dirname : It will resolve to your project folder.
+//Creating the Express server
+const app = express();
+
+//Set the view engine to ejs
+app.set("view engine", "ejs");
+
+//creat the express router
+const router=express.Router();
+
+//serve static fields in express
+//configuring middleware
+app.use(express.static(path.join(__dirname,"public"))); //then e.g. this will work on http://localhost:3000/images/firefox-icon.png
+app.use(express.urlencoded({ extended: false })); // use the middleware “express.urlencoded()” so that request.body retrieves the posted values
+
+//connecting to the sqlite database
+const db_name = path.join(__dirname, "data", "Vaccine.db");
+console.log("database fullname: "+db_name);
+const db = new sqlite3.Database(db_name, err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Successful connection to the database 'Vaccine.db'");
 });
 
+//Creating the tracker table (userID, Name, surname, date, vaccine, place)
+const sql_create = `CREATE TABLE IF NOT EXISTS tracker (
+  tracker_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+  Name VARCHAR(100) NOT NULL,
+  Surname VARCHAR(100) NOT NULL,
+  date DATE NOT NULL,
+  vaccine TEXT NOT NULL,
+  place TEXT NOT NULL
+);`;
+
+db.run(sql_create, err => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Successful creation of the 'tracker' table");
+  
+  //Database seeding
+  // use the identifier, ie: the 1 in the row inserted to prevent reinsertion of the seed over and over
+  const sql_insert=`INSERT INTO tracker (tracker_ID, Name, Surname, date, vaccine, place) VALUES
+  (1,'John','Doe','2020-01-01','Johnsons','Rondebosch');`;
+  db.run(sql_insert,(err)=>{
+    if (err){
+      return console.error(err.message);
+    }
+    console.log("Successful creation of 1 users");
+  });
+});
+
+//GET /
+router.get("/", function (req, res) {
+  const sql="SELECT * FROM tracker";
+  db.all(sql, [],(err,rows)=>{
+    if (err){
+      return console.error(err.message);
+    }
+    console.log("rows - "+rows.length);
+    //__dirname resolves to your project folder.
+    res.render(__dirname+"/views/index.ejs",{
+      status: "",
+      users: rows,
+    });
+    
+    //__dirname : It will resolve to your project folder.
+  });
+    //The sendfile method, on the other hand, simply sends a given file to the client, regardless of the type and contents of the file.
+    //render allows processing of variables but requires use of a templating engine e.g. name
+    //  res.sendFile(
+    //    path.join(__dirname + "/views/index.html")
+    //  );
+});
+  
 router.get("/about", function (req, res) {
   res.sendFile(path.join(__dirname + "/views/about.html"));
+});
+//to edit grab data from the html form need to use router.post 
+//"fname","sname","date","Vaxadmin","pvax"
+router.post("/", function (req, res) {
+  const user = [req.body.fname, req.body.sname,req.body.date,req.body.Vaxadmin,req.body.pvax];
+  const sql = "INSERT INTO tracker (Name, Surname, date, vaccine, place) VALUES (?,?,?,?,?)";
+  db.run(sql, user, (err) => {
+    console.log("added");
+
+    const sql="SELECT * FROM tracker";
+    db.all(sql, [],(err,rows)=>{
+      if (err){
+        return console.error(err.message);
+      }
+      console.log("rows - "+rows.length);
+      //__dirname resolves to your project folder.
+      res.render(__dirname+"/views/index.ejs",{
+        status: "",
+        users: rows,
+      });
+    });
+    
+  });
 });
 
 //add the router
 app.use("/", router);
-app.listen(process.env.port || 3000);
-
-console.log("Running at Port 3000");
+app.listen(3000, ()=>{
+  console.log("Running at Port 3000");
+});
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,108 +1,14 @@
-/*
-//Tutorial code and notes, uncomment to run
-const myHeading=document.querySelector('h1'); //querySelector() grabs a reference to your heading and then stores it in a variable called myHeading
-myHeading.textContent='Hello world changed!'; //then myHeading changes the textContent property (which is in this case is the content of the heading) to Hello world!.
-
-let stringVar='munchkin';
-let numVar=10;
-let boolVar=true;
-let arrVar=[1,stringVar,numVar,boolVar] // ref by arrVar[0] for item in first index, note: not sure if this is const or reference variables in array when giving array variables
-*/
-
-//Operators +-*/= hold as usual
-//Identity operator is ===. Equality operator remains == and variants. Q: differnece between equality 
-//! is 'not' - returns logical opposite note: for !== would be not equal
-//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators
-//Go to link for more expressions and operators
-
-/*//this is commented out for now buts it's an annoying pop-up
-let icecream='chocolate';
-if (icecream==='chocolate'){
-	alert('Yay, I love chocolate ice cream!');
-}else {
-	alert('Awwww, but chocolate is my favourite...');
-}
-
-//function demonstrating functions
-function multiply(num1, num2){
-	let result=num1*num2;
-	return result;
-}
-*/
-//Q: will variable still work if you don't use a specifier? var, let is not the only so what's the rule?
-
-
-//button work, uncomment in html too
-/*
-let secondHead=document.querySelector('h2');
-let myButton = document.getElementById('clickbut'); //get the button varaible
-function doSomething(){
-	let myName=prompt('Please enter your name.'); //prompt which alerts the user and stores the data the user enters after they click okay 
-	localStorage.setItem('name',myName); // storing it through an API called localStorage which allows the browser the retrieve the data later;
-	secondHead.textContent= 'Hello ' +myName; //now set the text content to whatever you like. Note, if the individual doesn't enter a name, in which case check for NULL. ie: if(!myName){ <call fucntion again> }else {<continue as here>} 
-}
-myButton.onclick=function(){
-	doSomething();
-}
-*/
-
-var n=1;
-var x=0;
-
-//for condensed code use
+//just validation now
 var n_string=["fname","sname","date","Vaxadmin","pvax"];
-var storageOb=[];
-
-/*
-//this is uncondensed code for the table update
-//This could be condensed into 2d array
-var names=[];
-var surnames=[];
-var dates=[];
-var vac=[];
-var place=[];
-function form_sub(){ 
-	let AddRown=document.getElementById('tbl');
-	let NewRow=AddRown.insertRow(1);
-//this could be condensed if 2d array of names
-	names[x]=document.getElementById('fname').value;
-	surnames[x]=document.getElementById('sname').value;
-	dates[x]=document.getElementById('date').value;
-	vac[x]=document.getElementById('Vaxadmin').value;
-	place[x]=document.getElementById('pvax').value;
-
-// check if condensable again
-	let cell1=NewRow.insertCell(0);
-	let cell2=NewRow.insertCell(1);
-	let cell3=NewRow.insertCell(2);
-	let cell4=NewRow.insertCell(3);
-	let cell5=NewRow.insertCell(4);
-
-	cell1.innerHTML=names[x];
-	cell2.innerHTML=surnames[x];
-	cell3.innerHTML=dates[x];
-	cell4.innerHTML=vac[x];
-	cell5.innerHTML=place[x];
-	n++;
-	x++;
-	document.getElementById('SRForm').reset();
-	return false;
-}
-*/
+var n_string=["Name","Surname","Date of vaccination","Vaccine administered","Place of vaccination"];
 
 function condensed_formsub(){
-	var AddRown=document.getElementById('tbl');
-	var NewRow=AddRown.insertRow(1);
 	var temp=[];
 	for (let i=0;i<n_string.length;i++){
-		temp[i]=document.getElementById(n_string[i]).value;
-		NewRow.insertCell(i).innerHTML=temp[i];
+		if(document.getElementById(n_string[i]).value==""){
+			alert(n_string[i]+" must be filled out");
+			return false;
+		}
 	}
-	storageOb[x]=temp;
-	n++;
-	x++;
-	document.getElementById('SRForm').reset();
-	return false;
+	return true;
 }
-//send in temp array to be evaluated for any info
-function validate(x){}
